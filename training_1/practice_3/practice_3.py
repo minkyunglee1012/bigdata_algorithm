eb85cd6027edb75ae88b8199b0e1546307ba7622
minkyunglee1012 <minkyunglee1012@gmail.com>
2024-02-14 11:29:40 +0900

    Add practice 1

--- training_1/practice_3/practice_3.py
+++ training_1/practice_3/practice_3.py
@@ -8,7 +8,7 @@ T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     arr = list(map(int, input().split()))
-    # 순열을 이용해서 숫자를 3개씩 나열하는 리스트 만든 후 각각의 합을 구하는 리스트를 만들자
+    # 조합을 이용해서 숫자를 3개씩 나열하는 리스트 만든 후 각각의 합을 구하는 리스트를 만들자
     perm_list = list(combinations(arr, 3))
 
     perm_sum = []

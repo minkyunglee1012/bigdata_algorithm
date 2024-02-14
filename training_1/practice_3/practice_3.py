c57486488427530fd5a52ba111c702cbd2703867
Lee Min Kyung <minkyunglee1012@gmail.com>
2024-02-14 11:36:15 +0900

    Update practice_3.py

--- training_1/practice_3/practice_3.py
+++ training_1/practice_3/practice_3.py
@@ -8,16 +8,31 @@ T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     arr = list(map(int, input().split()))
-    # 순열을 이용해서 숫자를 3개씩 나열하는 리스트 만든 후 각각의 합을 구하는 리스트를 만들자
-    perm_list = list(combinations(arr, 3))
+    # 조합을 이용해서 숫자를 3개씩 나열하는 리스트 만든 후 각각의 합을 구하는 리스트를 만들자
 
-    perm_sum = []
-    for i in range(len(perm_list)):
-        perm_sum.append(sum(perm_list[i]))
-    perm_sum = list(set(perm_sum))
+
+    def combination(array, n):
+        result = []
+        if n == 1:
+            return [[i] for i in array]
+
+        for i in range(len(array)):
+            val = array[i]
+            rest_comb = combination(array[i+1:], n-1)
+            for cb in rest_comb:
+                result.append([val] + cb)
+
+        return result
+
+    comb_list = combination(arr, 3)
+
+    com_sum = []
+    for i in range(len(comb_list)):
+        com_sum.append(sum(comb_list[i]))
+    com_sum = list(set(com_sum))
 
     for i in range(4):
-        perm_sum.remove(max(perm_sum))
+        com_sum.remove(max(com_sum))
 
-    ans = max(perm_sum)
+    ans = max(com_sum)
     print(f"#{test_case} {ans}")
